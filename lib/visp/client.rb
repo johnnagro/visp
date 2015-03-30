a90480dfb9ffcb1ef188f4d76bc015b41c3ffc17
Lars Gierth <larsg@systemli.org>
2015-03-30 06:39:00 +0200

    client: port the init script over

--- lib/visp/client.rb
+++ lib/visp/client.rb
@@ -5,11 +5,58 @@ require 'oj'
 
 module VISP
   class Client
+    Connection = Struct.new(:server, :direction, :ipv4, :ipv4_prefix) do
+      def outgoing?
+        direction == 'out'
+      end
+    end
+
     def initialize(cjdns, options)
+      @cjdns = cjdns
+      @options = options
     end
 
     def run
-      $stderr.puts 'VISP::Client#run'
+      conn = connections.first
+      net, gw = network(conn), gateway(conn)
+
+      ip "addr #{@cjdns.interface} #{conn[:ipv4]}"
+
+      if net == '0.0.0.0/0'
+        # traffic to UDP peers needs to stay outside the tunnel, add host routes
+        @options[:peers].each { |ipv4| ip "gw #{ipv4} #{@options[:original_gateway]}" }
+
+        # send all other traffic through the tunnel
+        ip "route #{@cjdns.interface} #{net}"
+        ip "gw default #{gw}"
+      else
+        ip "route #{@cjdns.interface}"
+      end
+    end
+
+    def connections
+      sh("#{@options[:dir]}/tools/iptunnel -t -1").each_line
+        .map { |line| Connection.new(*line.split(/\s+/)) }
+        .select(&:outgoing?)
+    end
+
+    def network(conn)
+      sh("ipcalc -n -b #{conn.ipv4}/#{conn.ipv4_prefix} | grep Network" +
+        " | tr -d '[[:space:]]' | cut -d':' -f2").strip
+    end
+
+    def gateway(conn)
+      sh("ipcalc -n -b #{conn.ipv4}/24 | grep HostMin" +
+        " | tr -d '[[:space:]]' | cut -d':' -f2").strip
+    end
+
+    def ip(args)
+      path = File.expand_path('../../../bin/visp-ip', __FILE__)
+      sh "sudo --non-interactive --reset-timestamp #{path} #{args}"
+    end
+
+    def sh(command)
+      `#{command}`
     end
 
     def maintain_tunnels
